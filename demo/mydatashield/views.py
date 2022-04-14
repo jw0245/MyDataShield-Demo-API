a84fa544fda569c31d13641b50312264b4d5e26c
gw0245 <xx6677as@gmail.com>
2022-04-14 18:18:44 +0900

    잘못된 외부 모듈 참조 삭제

--- demo/mydatashield/views.py
+++ demo/mydatashield/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-from numpy import var
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import render
